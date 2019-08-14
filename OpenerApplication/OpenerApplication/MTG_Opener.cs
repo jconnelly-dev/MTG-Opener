99cdbe39c642cfcc85936d16a1f3a8af24c830b4
Joseph Connelly <joseph_a_connelly@yahoo.com>
2019-08-14 09:52:23 -0600

    using .net windows form application for project. added a table with 4 columns, 3 buttons

---
+++ OpenerApplication/OpenerApplication/MTG_Opener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenerApplication
+{
+	public partial class MTG_Opener : Form
+	{
+		public MTG_Opener()
+		{
+			InitializeComponent();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Opener_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ OpenerApplication/OpenerApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenerApplication
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MTG_Opener());
+		}
+	}
+}
